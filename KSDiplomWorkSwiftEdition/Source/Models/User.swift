61f86e384ed844a2a611d6e03b00fec08f8210ef
SerhiiKostiuk <serhii.kostiuk@foreway.pro>
2017-04-28 16:11:19 +0300

    add user model

---
+++ KSDiplomWorkSwiftEdition/Source/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  KSDiplomWorkSwiftEdition
+//
+//  Created by Serhii Kostiuk on 4/28/17.
+//  Copyright © 2017 Serhii Kostiuk. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var defaultCategories: Array<Any> = []
+    
+    
+    
+}
