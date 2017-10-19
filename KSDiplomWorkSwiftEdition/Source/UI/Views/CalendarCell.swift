27021637d6b4f2e0af606322c7424c8882ff0d08
SerhiiKostiuk <serhii.kostiuk@foreway.pro>
2017-10-19 12:42:15 +0300

    add calendar cell

---
+++ KSDiplomWorkSwiftEdition/Source/UI/Views/CalendarCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarCell.swift
+//  KSDiplomWorkSwiftEdition
+//
+//  Created by Serhii Kostiuk on 7/26/17.
+//  Copyright © 2017 Serhii Kostiuk. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+
+
+}
